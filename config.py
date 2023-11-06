b405af44652ee261aea5abe80b027117911e2d6a
Akshit <120463129+sukkunmusic@users.noreply.github.com>
2023-11-06 17:04:50 +0530

    Update config.py
    Signed-off-by: Akshit <120463129+sukkunmusic@users.noreply.github.com>

--- config.py
+++ config.py
@@ -18,10 +18,10 @@ START_IMG = getenv("START_IMG", "https://te.legra.ph/file/f8ba75bdbb9931cbc8229.
 
 SESSION = getenv("SESSION", None)
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DevilsHeavenMF")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/FallenAssociation")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Friends_Forever_581")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/funny_joke_memes_comedy_videos")
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1356469075").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6306809961").split()))
 
 
 FAILED = "https://te.legra.ph/file/4c896584b592593c00aa8.jpg"
